4e8c8867e4f6d898638c99fa80fa7142f537419a
hansrocketry-hue <hansrocketry@gmail.com>
2026-03-22 14:36:46 +0900

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,3 +1,4 @@
+// 필요 라이브러리
 #include <Wire.h>
 #include <SPI.h>
 #include <SD.h>
@@ -5,52 +6,63 @@
 #include <math.h>
 #include <Adafruit_BMP280.h>
 
+// MPU6050 IMU 센서의 출력 데이터 구조체
+
 struct MPUData{
-  float AcX;
-  float AcY;
+  float AcX; // X 방향 가속도, g (9.81 m/s^2)
+  float AcY; 
   float AcZ;
-  float Tmp;
-  float GyX;
+  float Tmp; // 온도 'C
+  float GyX; // X 방향 각속도, deg/s
   float GyY;
   float GyZ;
 };
 
+// 서보 모터 정의
 Servo servo;
+const int SERVO_PIN = 14;
 
+// MPU6050 기본 I2C 주소
 const int MPU_ADDR = 0x68;
+
 // 1. 태스크 핸들러 변수 선언
 TaskHandle_t TaskHandle;
 SemaphoreHandle_t xMutex;
 struct MPUData* MPUAddr = nullptr;
 
+// 기압고도계 및 해수면 기압 정의
 Adafruit_BMP280 bmp;
-float seaLevelPressure = 1019.6;
+float seaLevelPressure = 1019.6; // 기상청 참고해서 그때의 해면기압 넣기
+float altitude; // 현재 해수면 기준 고도
 float height_ini;
-float altitude;
 
+// SD 카드 핀 정의
 const int CS = 5;
 const int SD_MOSI = 23;
 const int SD_MISO = 19;
 const int SD_SCK = 18;
 
+// SD 카드에 저장할 파일 저장
 File dataFile;
 
+// 현재 기체의 각도 (자이로 센서 적분 기준)
 float AngleX = 0;
 float AngleY = 0;
 float AngleZ = 0;
 
+// 마지막으로 IMU에서 값을 불러온 시점(millis 기준)
 unsigned long last_ms_imu = 0;
 
-const float LAUNCH_THERSHOLD = 50;
-const int SERVO_PIN = 14;
-const float FALL_MARGIN = 5;
-const int COUNT_LIMIT = 3;
+const float LAUNCH_THRESHOLD = 50; // 사출 기준 고도 (m)
+const float FALL_MARGIN = 5; // 최고 고도 기준 추락 허용 고도 (m) -> 최고 고도 기준 --m 이상 하강 시 사출 조건 충족
+const int COUNT_LIMIT = 3; // 최고 고도 기준 추락이 --번 감지되면 사출 조건 충족
 
-float maxHeight = 0;
-int fallCount = 0;
-bool isLaunched = false;
-bool isDeployed = false;
+float maxHeight = 0; // 최고 도달 고도 (m)
+int fallCount = 0; // 추락 감지 횟수
+bool isLaunched = false; // 발사 여부
+bool isDeployed = false; // 사출 여부
 
+// 시작 코드
 void setup() {
   Wire.begin(21, 22);
   xMutex = xSemaphoreCreateMutex();
@@ -62,9 +74,11 @@ void setup() {
   servo.attach(SERVO_PIN);
   servo.write(0);
 
+  height_ini = bmp.readAltitude(seaLevelPressure);
+
   // 2. 태스크 생성 시 핸들러 주소(&myTaskHandle) 전달
   xTaskCreatePinnedToCore(
-    sensoringTask,          // 태스크 함수
+    loggingTask,          // 태스크 함수
     "Task_Example",  // 이름
     10000,           // 스택 크기
     NULL,            // 파라미터
@@ -77,13 +91,17 @@ void setup() {
 void loop() {
   if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE) {
     // --- 임계 구역 (Critical Section) 시작 ---
-    checklaunch(altitude);
+
+    updateMPUData();
+    altitude = bmp.readAltitude(seaLevelPressure) - height_ini;
+
+    checklaunch(altitude); // 발사 여부 판정
     float g = sqrt(
       MPUAddr->AcX * MPUAddr->AcX +
       MPUAddr->AcY * MPUAddr->AcY +
       MPUAddr->AcZ * MPUAddr->AcZ
-    );
-    if (checkHeight(altitude, g) && isLaunched && !isDeployed){
+    ); // g 단위로 현재 총 가속도
+    if (checkHeight(altitude, g) && isLaunched && !isDeployed){ // 발사 이후, --번 이상의 추락 감지 시 사출
       Deploy();
     }
     // --- 임계 구역 끝 ---
@@ -91,12 +109,14 @@ void loop() {
   }
 }
 
+// 발사 감지
 void checklaunch(float h) {
-  if(!isLaunched && h > LAUNCH_THERSHOLD) {
+  if(!isLaunched && (h > LAUNCH_THRESHOLD)) {
     isLaunched = true;
   }
 }
 
+// 추락 감지
 bool checkHeight(float h, float g) {
   if(h > maxHeight) {
     maxHeight = h;
@@ -115,6 +135,7 @@ bool checkHeight(float h, float g) {
   return false;
 }
 
+// 사출
 void Deploy() {
   for(int i = 0; i<10; i++) {
     servo.write(90);
@@ -125,14 +146,11 @@ void Deploy() {
   isDeployed = true;
 }
 
-void sensoringTask(void *pvParameters) {
+// 데이터 로깅 멀티스레딩
+void loggingTask(void *pvParameters) {
   while(1) {
     if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE) {
       // --- 임계 구역 (Critical Section) 시작 ---
-      updateMPUData();
-      altitude = bmp.readAltitude(seaLevelPressure) - height_ini;
-      Serial.print("Alt:");
-      Serial.println(altitude);
       saveToSD();
       // --- 임계 구역 끝 ---
       xSemaphoreGive(xMutex); // 뮤텍스 반납
@@ -141,6 +159,7 @@ void sensoringTask(void *pvParameters) {
 
   }
 }
+
 
 #pragma region MPU6050
 void beginMPU(){
@@ -271,7 +290,6 @@ void beginBmp(){
     Adafruit_BMP280::FILTER_X16,      // Filtering
     Adafruit_BMP280::STANDBY_MS_500   // Standby time
   );
-  height_ini = bmp.readAltitude(seaLevelPressure); // define initial altitude
 
   Serial.println("Bmp Start");
 }
